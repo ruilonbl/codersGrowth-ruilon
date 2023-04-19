75eed452306df2569e3035ce0c2d53a0aeb52a99
ruilonbl <ruilonbl@gmail.com>
2023-04-19 11:57:03 -0300

    mudei lista emm memoria para singleton

---
+++ model/ListSingleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho01.model
+{
+    public sealed class ListSingleton
+    {
+        //private ListSingleton() { }
+        private static BindingList<Pessoa> lista;
+        static int i = 1;
+        public static BindingList<Pessoa> Lista()
+        {
+            if(lista == null)
+            {
+                lista = new BindingList<Pessoa>();
+            }
+            return lista;
+           // set { lista = value; }
+        }
+        public static int cont(bool atualiza)
+        {
+            if(atualiza)
+            {
+                return i;
+            }
+            return i++;
+        }
+    }
+}
