f6ace16f5293e478d35eb325596caec8c8cb8d14
ruilonbl <ruilonbl@gmail.com>
2023-05-24 13:23:57 -0300

    endpoints criados

---
+++ codersGrowth.web/Program.cs
@@ -0,0 +1,22 @@
+﻿using codersGrowth.Infra.Data;
+using trabalho01.crud;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRepositorio, RepositorioLinq2DB>();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
