81c010aebb53f19903a2335814b37bc62488c5f8
ruilonbl <ruilonbl@gmail.com>
2023-05-26 11:45:16 -0300

    iniciando implementção de listagem

--- codersGrowth.web/wwwRoot/controller/academia.controller.js
+++ codersGrowth.web/wwwRoot/controller/academia.controller.js
@@ -3,6 +3,13 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel"
  ], function (Controller) {
     "use strict";
+    const uri = 'https://localhost:7020/api/alunos';
     return Controller.extend("sap.ui.demo.academia.controller.App", {
+      getItems : function (){
+      fetch(uri)
+      .then(reponse => reponse.json())
+      .then(data => _displayItems(data))
+      }
     });
+    
  });
